92be3e454eeffa9a478e3a5f2ff728ea5029e9a5
Dimosthenis Botsaris <botsaris.d@gmail.com>
2017-09-23 10:03:03 +0300

    add facebook service

---
+++ src/networks/facebook.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+module.exports = class InstagramService {
+  constructor(avatarService) {
+    this._avatarService = avatarService;
+  }
+
+  _createErrorMessage(error, defaultMsg) {
+    return error && error.message ? error.message : defaultMsg;
+  }
+
+  getAvatar(username) {
+    return this._avatarService.getImage(`https://graph.facebook.com/${username}/picture`, 'facebook')
+      .then(response => response)
+      .catch(error => Promise.reject(error));
+  }
+};
