dfcf8f40f678a9215df80cefb50912825c016e22
eldimious <botsaris.d@gmail.com>
2018-11-27 20:23:12 +0200

    instagram with async

--- src/networks/instagram.js
+++ src/networks/instagram.js
@@ -1,14 +1,14 @@
-const Promise = require('bluebird');
-
 module.exports = class InstagramService {
   constructor(avatarService) {
     this._avatarService = avatarService;
   }
 
-  getAvatar(username) {
-    return this._avatarService.findImage(`https://www.instagram.com/${username}/`, 'instagram')
-      .then(imageUrl => this._avatarService.getImage(imageUrl, 'instagram'))
-      .then(response => response)
-      .catch(error => Promise.reject(error));
+  async getAvatar(username) {
+    try {
+      const imageUrl = await this._avatarService.findImage(`https://www.instagram.com/${username}/`, 'instagram');
+      return this._avatarService.getImage(imageUrl, 'instagram');
+    } catch (error) {
+      throw error;
+    }
   }
 };
