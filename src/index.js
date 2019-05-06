92a4fa7e06e6ed8ebb6ffd22e46fb5e3db492c98
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-05-06 11:37:26 +0300

    Working with gmail provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const tumblr = require('./providers/tumblr');
 const twitter = require('./providers/twitter');
 const vimeo = require('./providers/vimeo');
 const youtube = require('./providers/youtube');
+const gmail = require('./providers/gmail');
 
 class NetworkAvatarPicker {
   constructor() {
@@ -15,6 +16,7 @@ class NetworkAvatarPicker {
     this.twitter = twitter.init();
     this.vimeo = vimeo.init();
     this.youtube = youtube.init();
+    this.gmail = gmail.init();
     if (new.target === NetworkAvatarPicker) {
       Object.freeze(this);
     }
